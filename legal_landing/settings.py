0e6fa4c08d9aa9df370f32845d49d8a3c481c73c
Mirodil Ortikov <ortikov.m97@gmail.com>
2026-07-06 10:30:25 +0500

    Add STATIC_ROOT for server static files collection

--- legal_landing/settings.py
+++ legal_landing/settings.py
@@ -141,6 +141,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'landing' / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
